401d3d9c3633858f6e14b1cb2bfcf5dfd4246f72
molybdenum1 <kozelden4ik@gmail.com>
2022-11-10 01:06:37 +0200

    send msg end photos from cli

--- 04TelgramSender/index.js
+++ 04TelgramSender/index.js
@@ -1,45 +1,49 @@
-const TelegramBot = require('node-telegram-bot-api');
-const { program } = require('commander');
+const TelegramBot = require("node-telegram-bot-api");
+const { program } = require("commander");
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '5590328138:AAF-89MgPjEQQLeubUJnJ1ra5UlrBuuemAo';
+const token = "5590328138:AAF-89MgPjEQQLeubUJnJ1ra5UlrBuuemAo";
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
+bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  if (msg.text.toString().toLowerCase().includes('bye')) {
+  if (msg.text.toString().toLowerCase().includes("bye")) {
     bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
   }
-  if (msg.text.toString().toLowerCase().includes('hi')) {
-      bot.sendMessage(msg.chat.id, "хелоу");
-  }
-  if (msg.text.toString().toLowerCase().includes('hello')) {
+  if (msg.text.toString().toLowerCase().includes("hi")) {
     bot.sendMessage(msg.chat.id, "хелоу");
   }
-  // send a message to the chat acknowledging receipt of their message
-  //bot.sendMessage(chatId, 'ху'+ msg.text.slice(1));
+  if (msg.text.toString().toLowerCase().includes("hello")) {
+    bot.sendMessage(msg.chat.id, "хелоу");
+  }
+  if (msg.text.toString().toLowerCase().includes("hello")) {
+    console.log(msg);
+    bot.sendPhoto(
+      msg.chat.id,
+      "https://www.rbc.ua/static/img/t/o/tornado_01_650x410.jpg"
+    );
+  }
 });
 
+program.name("sender from cli").description("CLI to telegram").version("0.1.0");
 
 program
-  .option('-m')
-  .option('-p')
+  .command("sender")
+  .description("Send your message from cli to telegram bot")
+  .argument("<string>", "string to send")
+  .option("--m", "sendind text message")
+  .option("--p", "sending a photo from web or your pc")
+  .action(async (str, options) => {
+    if (options.m) {
+      await bot.sendMessage(389429451, str);
+      process.exit();
+    }
+    if (options.p) {
+      await bot.sendPhoto(389429451, str);
+      process.exit();
+    }
+  });
 
 program.parse();
-
-const options = program.opts();
-console.log(options);
-if(program.args){
-  if(options.m){
-    bot.sendMessage(389429451, program.args.join(' '))
-    process.exit()
-  }
-  if(options.p){
-    console.log(program.args);
-    bot.sendPhoto(389429451, 'https://www.rbc.ua/static/img/t/o/tornado_01_650x410.jpg')
-    process.exit()
-  }
-}
-  
