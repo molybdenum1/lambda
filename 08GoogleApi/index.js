deb75e88ae8ecaff66d12d7f72e3fa6e1b497551
molybdenum1 <kozelden4ik@gmail.com>
2022-11-16 00:04:28 +0200

    uploading on goole disk

--- 08GoogleApi/index.js
+++ 08GoogleApi/index.js
@@ -1,3 +1,38 @@
-
+const fs = require('fs');
+const {google} = require('googleapis');
 
 const GOOGLE_API_ID = '1nsrQ71wg3uWb6OWH607e0yozyKGMSwqk';
+
+async function uploadFiles() {
+    try {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './drive-uploader.json',
+            scopes:  'https://www.googleapis.com/auth/drive'
+        })
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        })
+        const fileMetadata = {
+            name: 'monkey.jpg',
+            parent: [GOOGLE_API_ID]
+        };
+        const media = {
+            mimeType: 'image/jpeg',
+            body: fs.createReadStream('monkey.jpg'),
+            
+        };
+
+        const file = await driveService.files.create({
+            resource: fileMetadata,
+            media: media,
+            fields: 'id',
+        });
+        console.log('File Id:', file.data.id);
+        return file.data.id;
+    } catch (error) {
+        console.log('upload file error', error);
+    }
+}
+
+uploadFiles().then(data => console.log(data))
