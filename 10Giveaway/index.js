052ef7a37445c2bfef921aa707e7f50025ceb0ab
molybdenum1 <kozelden4ik@gmail.com>
2022-11-28 00:14:22 +0200

    search for existing in all files

--- 10Giveaway/index.js
+++ 10Giveaway/index.js
@@ -10,13 +10,33 @@ function uniqueValues() {
             data.push(...str.split('\n'))
         }
         // 
-        // let set = new Set(sort(data));
-        console.log(sort(data));
-        // console.log(set);
+        let set = new Set(sort(data));
+        // console.log(sort(data));
+        console.log(set);
+        return set;
     } catch (error) {
         console.log(error);
     }
 }
 
+function existInAllFiles(arrayOfStrings) {
+    let arrOfExist = [];
+    for (let i = 0; i < arrayOfStrings.length; i++) {
+        let iterator = 0
+        for(let j = 0; j <= 19; i++){
+            let str = fs.readFileSync(`./files/out${j}.txt`, {encoding: 'utf8'});
+            if(str.includes(arrayOfStrings[i])){
+                iterator++
+            }
+        }
+        if(iterator === 20){
+            arrOfExist.push(arrayOfStrings[i])
+        }
+        
+    }
+    console.log(arrOfExist);
+}
 
-uniqueValues()
+
+let unique = uniqueValues();
+existInAllFiles(...unique);
