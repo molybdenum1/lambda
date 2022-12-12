dd2a7d3049063c5a5993fa9dfa17ae1cdefed449
molybdenum1 <kozelden4ik@gmail.com>
2022-12-12 22:23:48 +0200

    full api

--- 13ip/getCvsData/getData.js
+++ 13ip/getCvsData/getData.js
@@ -1,6 +1,9 @@
-import fs from 'fs';
+import fs from "fs";
 
 export default function () {
-    let data = fs.readFileSync('C:/Users/kozel/proga/courses/lambda/13ip/loc.CSV', 'utf-8');
-    return data.split('\r\n');
+  let data = fs.readFileSync(
+    "C:/Users/kozel/proga/courses/lambda/13ip/loc.CSV",
+    "utf-8"
+  );
+  return data.split("\r\n");
 }
--- 13ip/index.js
+++ 13ip/index.js
@@ -1,40 +1,31 @@
-// let express = require('express');
-// const ip  = require('./getIp/index');
+import express from "express";
 import getCountryfromIP from "./getCountry/getCountry.js";
-// let app = express();
+import getMyIp from "./getMyIp/getMyIp.js";
 
-// app.get('/', (req, res) => {
-//     // const ipAddresses = req.header('x-forwarded-for');
-//     let ip = '';
-//     if (req.headers['x-forwarded-for']) {
-//         ip = req.headers['x-forwarded-for'].split(",")[0];
-//     } else if (req.connection && req.connection.remoteAddress) {
-//         ip = req.connection.remoteAddress;
-//     } else {
-//         ip = req.ip;
-//     }console.log("client IP is *********************" + ip);
-//     res.send(ip);
-//     // console.log();
-// })
+let app = express();
 
-// app.listen(5000, () => {
-//     console.log('Running on 5000 port');
-// })
-// // ip.getIP()
+app.get("/myIp", (req, res) => {
+  let ip = getMyIp();
+  res.send(getCountryfromIP(ip));
+});
 
+app.get("/ipFromParams", (req, res) => {
+  let ip = req.query?.ip;
+  res.send(getCountryfromIP(ip));
+});
 
-// console.log(data);
+app.listen(5000, () => {
+  console.log("Running on 5000 port");
+});
 
-
-console.log(getCountryfromIP('77.83.248.211'));//romania
-console.log(getCountryfromIP('45.234.156.1'));//chile
-console.log(getCountryfromIP('23.43.23.15'));//uk
-console.log(getCountryfromIP('185.182.120.34')); //Armenia
-console.log(getCountryfromIP('45.177.176.23')); //Mexico 
-console.log(getCountryfromIP('5.44.80.51')); //Turkey
-console.log(getCountryfromIP('91.149.48.22')); //Norway
-console.log(getCountryfromIP('83.229.33.3')); //Spain
-console.log(getCountryfromIP('203.24.108.65')); //Cyprus
-console.log(getCountryfromIP('89.28.176.5')); //Ireland
-console.log(getCountryfromIP('91.193.172.165')); //
-
+// console.log(getCountryfromIP('77.83.248.211'));//romania
+// console.log(getCountryfromIP('45.234.156.1'));//chile
+// console.log(getCountryfromIP('23.43.23.15'));//uk
+// console.log(getCountryfromIP('185.182.120.34')); //Armenia
+// console.log(getCountryfromIP('45.177.176.23')); //Mexico
+// console.log(getCountryfromIP('5.44.80.51')); //Turkey
+// console.log(getCountryfromIP('91.149.48.22')); //Norway
+// console.log(getCountryfromIP('83.229.33.3')); //Spain
+// console.log(getCountryfromIP('203.24.108.65')); //Cyprus
+// console.log(getCountryfromIP('89.28.176.5')); //Ireland
+// console.log(getCountryfromIP(')); //
