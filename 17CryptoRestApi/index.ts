41d93681b2170defaba3af1bc14375e5c09888ac
molybdenum1 <kozelden4ik@gmail.com>
2023-02-26 13:20:22 +0200

    smth

--- 17CryptoRestApi/index.ts
+++ 17CryptoRestApi/index.ts
@@ -1,5 +1,6 @@
 import express, {Application, Request, Response} from 'express';
 import { apiLinks } from './data/links';
+import { conn } from './db';
 import { getCrypto } from './getCrypto';
 
 const app: Application = express();
@@ -7,14 +8,19 @@ const PORT = 5050;
 
 app.use(express.json());
 app.get('/getCrypto', (req: Request, res: Response) => {
-    let {coin, market, period} = req.params;
-    let response = []
+    // let {coin, market, period} = req.params;
+    // let response = []
     
     res.send('alal');
 })
 
 app.listen(PORT, async () => {
-    console.log( await getCrypto('', '', 'cucoin').then(res => res))
+    // console.log( await getCrypto('', '', 'cucoin').then(res => res))
+    conn.connect(err => {
+        if(err) throw err;
+        console.log('db connected');
+        
+    })
     console.log(`Server is running on localhost://${PORT}`);
     
 })
