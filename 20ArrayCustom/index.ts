4e14839901560dc793ca19c30e140c025391ace9
molybdenum1 <kozelden4ik@gmail.com>
2023-03-13 21:10:53 +0200

    more methods

--- 20ArrayCustom/index.ts
+++ 20ArrayCustom/index.ts
@@ -3,93 +3,138 @@
 // chunked
 // distinctBy
 // filter +
-// filterIndexed
+// filterIndexed +
 // filterNot +
 // find +
-// findLast
-// flatten
-// fold
+// findLast +
+// flatten + 
+// fold +
 // maxBy
 // minBy
 // count + (с селектором — у нас есть массив обьектов, внутри обьекта ключ, например, population).
 // И мы хотим посчитать общее население по всем обьектам. (аналог countBy).
 // groupBy - оба
 
-
-interface Array<T>{
-    multiply(facto: number): T[];
-    average(): number,
-    myFilter(condition: (num: number) => boolean): T[],
-    filterIndexed(): number[],
-    filterNot(predicate: (value: T) => boolean): T[],
-    findFirst(predicate: (value: T) => boolean): number | null,
-    fold<U>(initialValue: U, operation: (accumulator: U, element: T) => U): U,
-    count(predicate: (value:T, index: number, array: T[]) => boolean): number,
+interface Array<T> {
+  multiply(facto: number): T[];
+  average(): number;
+  myFilter(condition: (num: number) => boolean): T[];
+  filterIndexed(predicate: (index: number, value: T) => boolean): T[];
+  filterNot(predicate: (value: T) => boolean): T[];
+  findFirst(predicate: (value: T) => boolean): number | null;
+  findLast(predicate: (value: T) => boolean): T | null;
+  fold<U>(initialValue: U, operation: (accumulator: U, element: T) => U): U;
+  flatten(): Array<T>;
+  count(predicate: (value: T, index: number, array: T[]) => boolean): number;
 }
 
-Array.prototype.multiply = function(factor = 2): number[] {
-    const result: number[] = [];
-    for(let i = 0; i < this.length; i++){
-        result.push(this[i]* factor)
+Array.prototype.multiply = function (factor = 2): number[] {
+  const result: number[] = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(this[i] * factor);
+  }
+
+  return result;
+};
+
+Array.prototype.average = function (): number {
+  return this.reduce((a, b) => a + b, 0) / this.length;
+};
+
+Array.prototype.myFilter = function (condition) {
+  const filteredArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (condition(this[i])) {
+      filteredArray.push(arr[i]);
     }
+  }
+  return filteredArray;
+};
 
-    return result;
-}
-
-Array.prototype.average = function(): number {
-    return this.reduce((a,b) => a+b, 0)/this.length;
-}
-
-Array.prototype.myFilter = function(condition) {
-    const filteredArray = [];
-    for(let i = 0; i < this.length; i++){
-        if(condition(this[i])){
-            filteredArray.push(arr[i]);
-        }
+Array.prototype.filterIndexed = function (
+  predicate: (index: number, value: number) => boolean
+): number[] {
+  const filteredArr = [];
+  arr.forEach((value, index) => {
+    if (predicate(index, value)) {
+      filteredArr.push(value);
     }
-    return filteredArray;
-}
+  });
+  return filteredArr;
+};
 
-// Array.prototype.filterIndexed = function(): number[] {
-//   return this
-//}
+Array.prototype.filterNot = function (predicate) {
+  return this.filter((value: any) => !predicate(value));
+};
 
-Array.prototype.filterNot = function(predicate) {
-    return this.filter((value: any) => !predicate(value))
-}
-
-Array.prototype.findFirst = function(predicate): number | null {
-    const found = this.find(predicate);
-    return found? found : null;
-}
-Array.prototype.count = function(predicate) {
-    return this.reduce((counter, value, index, array) => {
-        if(predicate(value, index, array)) {
-            return counter + 1;
-        }
-        return counter;
-    }, 0)
-}
-
-Array.prototype.fold = function<T,U>(initialValue: U, operation: (accumulator: U, element: T) => U) {
-    let accumulator = initialValue;
-    for (let i = 0; i < this.length; i++) {
-        accumulator = operation(accumulator, this[i]);
+Array.prototype.findFirst = function (predicate): number | null {
+  for (let i = 0; i <= this.length; i++) {
+    const value = this[i];
+    if (predicate(value)) {
+      return value;
     }
-    return accumulator;
-}
+  }
+  return null;
+};
+Array.prototype.findLast = function (predicate): number | null {
+  for (let i = this.length - 1; i >= 0; i--) {
+    const value = this[i];
+    if (predicate(value)) {
+      return value;
+    }
+  }
+  return null;
+};
 
+Array.prototype.flatten = function (): any {
+  let flattenedArray = [];
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+    if (Array.isArray(element)) {
+      flattenedArray = flattenedArray.concat(element.flatten());
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray.sort((a,b) => a - b);
+};
 
+Array.prototype.count = function (predicate) {
+  return this.reduce((counter, value, index, array) => {
+    if (predicate(value, index, array)) {
+      return counter + 1;
+    }
+    return counter;
+  }, 0);
+};
 
-const arr: number[] = [2,4,5,6]
-console.log(arr.multiply(3));
-console.log(arr.average());
-console.log(arr.myFilter((num) => num % 2 !== 0));
-console.log(arr.filterNot((num) => num % 2 !== 0));
-console.log(arr.findFirst((num) => num % 2 === 0));
-console.log(arr.count((num) => num % 2 === 0));
-console.log(arr.fold(1, (accumulator, element) => accumulator * element));
+Array.prototype.fold = function <T, U>(
+  initialValue: U,
+  operation: (accumulator: U, element: T) => U
+) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    accumulator = operation(accumulator, this[i]);
+  }
+  return accumulator;
+};
 
+const arr: number[] = [1, 2, 3, 4, 5];
+console.log("Array by default " + arr);
+console.log("Multiply: " + arr.multiply(3));
+console.log("Average: " + arr.average());
+console.log("Filter: " + arr.myFilter((num) => num % 2 !== 0));
+console.log(
+  "Filter indexed: " +
+    arr.filterIndexed((index, value) => index % 2 === 0 && value % 2 === 0)
+);
 
+console.log("Not Filter: " + arr.filterNot((num) => num % 2 === 0));
+console.log("FirstEl : " + arr.findFirst((num) => num % 2 === 0));
+console.log("LastEl : " + arr.findLast((num) => num % 2 === 0));
+console.log("Flatten : " + [1,2,[2, [33,1], 2], 9].flatten());
 
-
+console.log("Count: " + arr.count((num) => num % 2 === 0));
+console.log(
+  "Fold: " + arr.fold(1, (accumulator, element) => accumulator * element)
+);
